3efc5c616a3bf0e239822836e091c13a38a8abf0
grshin <grshin@sptek.co.kr>
2021-06-22 13:10:50 +0900

    K8S Manifest 작업

--- src/main/java/net/sansae/TodoDemo/model/TodoItem.java
+++ src/main/java/net/sansae/TodoDemo/model/TodoItem.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class TodoItem {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String category;
     private String name;
